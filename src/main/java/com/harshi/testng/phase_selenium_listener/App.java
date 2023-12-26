04116f922d6eb2d6e336a7373e0a72a67607db00
harshithanap <ashwathharshitha0@gmail.com>
2023-12-26 15:22:41 +0530

    project setup for listener

---
+++ src/main/java/com/harshi/testng/phase_selenium_listener/App.java
@@ -0,0 +1,13 @@
+package com.harshi.testng.phase_selenium_listener;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/harshi/testng/testng/listener/TestClass.java
@@ -0,0 +1,21 @@
+package com.harshi.testng.testng.listener;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(TNListener.class)
+public class TestClass {
+
+	@Test
+	public void testOne() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testTwo() {
+		assertTrue(false);
+	}
+}
+
